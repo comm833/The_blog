8a49c40c90d626b35c8086d2f3cff3daf6d1195c
comm833 <adekomiadeleke@gmail.com>
2022-02-19 23:39:16 +0100

    going live

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,12 @@
 from atexit import register
+from os import name
 from pdb import post_mortem
 from  django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 
 register = template.Library()
 
@@ -22,3 +26,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
